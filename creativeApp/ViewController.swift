4437c4239736f4f166519d58a7c0157bcb31f299
Claire McGuire <claireviolet05@icloud.com>
2023-09-01 09:09:40 -0500

    Initial Commit

---
+++ creativeApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  creativeApp
+//
+//  Created by CLAIRE MCGUIRE on 9/1/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
